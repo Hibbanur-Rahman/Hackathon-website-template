5ef8876414f7b3c4d2f37b8b1aceeb26526611c0
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-01-18 23:26:02 +0530

    change the merging

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var typed=new Typed(".typing",{
     strings:["Win Awesome prizes","24 hours creation","Elevate Your Tech Quotient","Navigating the Tech Cosmos"],
     typeSpeed:100,
@@ -7,14 +6,4 @@ var typed=new Typed(".typing",{
 })
 
 const typedCursor= document.querySelector('.typed-cursor');
-=======
-var typed=new Typed(".typing",{
-    strings:["Win Awesome prizes","24 hours creation","Elevate Your Tech Quotient","Navigating the Tech Cosmos"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
-
-const typedCursor= document.querySelector('.typed-cursor');
->>>>>>> 3845e1b52a04cc6654427b12a1f70ab85313069f
 typedCursor.innerHTML='';
