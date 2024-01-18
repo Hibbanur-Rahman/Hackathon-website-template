2c35f86cbecf8cf47c383f599fb9153c2a5513e6
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-01-18 23:11:53 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed=new Typed(".typing",{
+    strings:["Win Awesome prizes","24 hours creation","Elevate Your Tech Quotient","Navigating the Tech Cosmos"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+const typedCursor= document.querySelector('.typed-cursor');
+typedCursor.innerHTML='';
